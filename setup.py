aca660003cef783c1f18cb3f91c2100d88e84bfb
Gsebs <109115487+Gsebs@users.noreply.github.com>
2022-07-12 15:54:06 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name="Data_Science",
+  author="Gerald Sebastian",
+  author_email="gsebs70@gmail.com",
+  license="MIT license",
+  discription="This is my math library",
+  packages=["Data_Science"]
+)
